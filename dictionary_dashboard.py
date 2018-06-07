3de1c9b67ff1289022b1c60e17ac3a3de4fc25bb
Utkarsh Saraswat <saraswatisdaniel@gmail.com>
2018-06-07 11:08:26 +0530

    dictionary_edited

--- dictionary_dashboard.py
+++ dictionary_dashboard.py
@@ -1,5 +1,17 @@
 # dictionary file
 
-Input1 # input string 1
-Input2 # input string 1
-desired_cell #desired cell in the  file
+
+find_cell(file_name,sheet_name,string)
+##function to find and return coordinates of a cell having given
+##value(name) from a sheet in a file
+
+find_val(product,parameter,file_name,sheet_name)
+##function to find and return value of a cell corresponding to given
+##parameter of a given product
+
+find_val(product,parameter,file_name,sheet_name)
+##function to find and return value of a cell corresponding to given
+##parameter of a given product
+
+rowval
+#array carrying value of a column titled products

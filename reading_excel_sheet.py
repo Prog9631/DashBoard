6ddd25bf6910a819a2eec60abc555cce1783ee25
Utkarsh Saraswat <saraswatisdaniel@gmail.com>
2018-06-07 11:01:09 +0530

    rowval

--- reading_excel_sheet.py
+++ reading_excel_sheet.py
@@ -5,21 +5,16 @@ import readsheet_functions as rsf
 
 file = 'data_sheet.xlsx'
 sheet = 'Sheet3'
+
 #------------------------------------------------
 
-##if save1[0]>save2[0] :
-##    cord.append(save1[0])
-##    cord.append(save2[1])
-##else:
-##    cord.append(save2[0])
-##    cord.append(save1[1])
 workbook = xl.open_workbook(file)
 worksheet = workbook.sheet_by_name(sheet)
-rownum=[]        
+rowval=[]        
 cell0=rsf.find_cell(file,sheet,"Products")
 
 for r in range (cell0[0]+1,worksheet.nrows):
     v = worksheet.cell(r,cell0[1])
-    rownum.append(v.value)
+    rowval.append(v.value)
         
-##print("value:",desired_cell[2])
+print(rowval)

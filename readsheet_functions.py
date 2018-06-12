0008c442803352cb108f5e4b63176d14f675bc68
Utkarsh Saraswat <saraswatisdaniel@gmail.com>
2018-06-12 10:36:25 +0530

    Modifications in functions
    Implemented levenstein strinf distance to even detect abbrevations and incorrect spellings in strings.

--- readsheet_functions.py
+++ readsheet_functions.py
@@ -1,6 +1,9 @@
 import xlrd as xl
 from xlrd import open_workbook as op
 from xlutils.copy import copy
+
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 #------------------------------------------------
     
 def find_cell(file_name,sheet_name,string,i=0,j=0):
@@ -8,35 +11,47 @@ def find_cell(file_name,sheet_name,string,i=0,j=0):
     worksheet = workbook.sheet_by_name(sheet_name)
     save1 = []
     temp =0
+    string = string.lower()
+    ##            column = worksheet.col_values(c)
+    cr = 0
     for r in range (i,worksheet.nrows):
         for c in range(j,worksheet.ncols):
-            column = worksheet.col_values(c)
             v = worksheet.cell(r,c)
-##            print(v)
-            if(v.value==string):
-                temp =1
-##                print(v)
-                save1.append(r)
-                save1.append(c)
+            if(isinstance(v.value,str)==True):
+                param = v.value
+                param = param.lower()
+                fr = fuzz.ratio(string,param)
+                if (fr>50 and fr>cr):
+                    cr = fr
+                    temp =1
+                    a = r
+                    b = c
+    save1.append(a)
+    save1.append(b)
+
     if temp == 0:
         print("string not found")
-        return 0
+##        return 0
     else:
         return save1
 
 def find_val(file_name,sheet_name,product,parameter1,parameter2='NULL'):
+  
     cpro = find_cell(file_name,sheet_name,product)
-    if (parameter1!='NULL'):
-        cpar1 = find_cell(file_name,sheet_name,parameter1)
-    else:
-        cpar1 = [0,0]
-
-    cpar2 = find_cell(file_name,sheet_name,parameter2,cpar1[0],cpar1[1])
-    
+    cpar1 = find_cell(file_name,sheet_name,parameter1)
+      
     workbook = xl.open_workbook(file_name)
     worksheet = workbook.sheet_by_name(sheet_name)
+
     a = []
-    a.append(cpro[0])
-    a.append(cpar2[1])
+    
+
+    if (parameter2!='NULL'):
+        cpar2 = find_cell(file_name,sheet_name,parameter2,cpar1[0],cpar1[1])
+        a.append(cpro[0])
+        a.append(cpar2[1])
+    else:
+        a.append(cpro[0])
+        a.append(cpar1[1])
     aval = worksheet.cell(a[0],a[1]).value
     return aval

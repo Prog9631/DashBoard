278b25d7313f190c44c4049103f3d26854da5ddb
Parag <paragkale96@gmail.com>
2018-06-12 10:37:53 +0530

    Clock, Calender added

--- Dictionery.py
+++ Dictionery.py
@@ -1,4 +1,13 @@
+import openpyxl
+wb = openpyxl.load_workbook('data_sheet.xlsx')
+sheet = wb['Sheet3']
+Words = {"Production", "production"}
 
-Words = { "prd" : {"production", "Production"}, "dspch" : {"dispatch", "Dispatch", "Dispatch/Sales"}}
+for colNum in range(1, sheet.max_column):
+    stringinexcel = sheet.cell(row=1, column=colNum).value
+    #print(stringinexcel)
+    if stringinexcel in Words:
+            print("yes")
+        
 
-print(Words["prd"])
+
